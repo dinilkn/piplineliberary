5dee8e7a053b8a14cc19a1036eca0d4cd42ce3af
dinilkn <39798802+dinilkn@users.noreply.github.com>
2018-07-10 11:02:18 -0700

    Update pipline.groovy

--- vars/pipline.groovy
+++ vars/pipline.groovy
@@ -9,29 +9,20 @@
             try {
          
 		   stage ('Clone') {
-                   sh "echo 'checkout'"
+                   sh "echo checkout"
 			 git url: 'https://github.com/dinilkn/jenkinSonarTestApp.git'
                 }
 		
                 stage ('rename text') {
-                     sh "mv  newfile modified1"			
-		    
-			
-			withCredentials([usernamePassword(credentialsId: 'de97785c-b80c-4e7d-8f05-60377dc48cde',
-							  passwordVariable: 'dinilkumar12', usernameVariable: 'dinilkn')]) {
+                        sh "mv  newfile modified1"			
 			 
 			 sh "git config --global user.email 'dinilkumar@gmail.com'"
   			 sh "git config --global user.name 'dinilkn'"
     			 sh "git add ."
-		   	 sh "git commit -am 'comment'"			
-   		    	 sh "git push origin master"
-			//  sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@https://github.com/dinilkn/jenkinSonarTestApp.git')
+		   	 sh "git commit -am 'comment'"		
+   		    	
+			 sh "git push origin master"
 			}
-		
-		     
-                }
-			    
-			   
 			   
 			   
             } catch (err) {
